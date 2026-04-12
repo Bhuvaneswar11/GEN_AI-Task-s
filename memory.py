2331e92321fe07cc15f6bafa69d79c5717ff7af7
Bhuvaneswar11 <126778626+Bhuvaneswar11@users.noreply.github.com>
2026-04-12 15:17:50 +0530

    Delete memory.py

--- memory.py
+++
@@ -1,6 +0,0 @@
-from langchain.memory import ConversationBufferMemory
-
-memory = ConversationBufferMemory()
-
-memory.save_context({"input": "Hi"}, {"output": "Hello"})
-print(memory.load_memory_variables({}))
